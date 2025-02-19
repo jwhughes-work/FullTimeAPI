9bdce07b5992924772f54289d32c4b37baf91f68
Josh Hughes <Hughes.j@millfieldschool.com>
2025-02-19 18:07:59 +0000

    updates to net 9

--- FullTimeAPI/Program.cs
+++ FullTimeAPI/Program.cs
@@ -17,12 +17,8 @@ builder.Services.AddLogging();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
